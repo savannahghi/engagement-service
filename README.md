5cfab4516ab8665d9c29f42660dbfbb9bf75632f
Mike Farad <mike18farad@gmail.com>
2020-10-14 10:20:25 +0300

    [ci skip] chore: Service SetUp

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# Feed Service
+# Debug micro-service
 
+[![pipeline status](https://gitlab.slade360emr.com/go/debug/badges/develop/pipeline.svg)](https://gitlab.slade360emr.com/go/debug/-/commits/develop)
+[![coverage report](https://gitlab.slade360emr.com/go/debug/badges/develop/coverage.svg)](https://gitlab.slade360emr.com/go/debug/-/commits/develop)
+
+A library and service with debug and monitoring helpers e.g a debug endpoint, healthcheck endpoint etc.
+
+## Environment variables
+
+For local development, you need to *export* the following env vars:
+
+```bash
+# Google Cloud Settings
+export GOOGLE_APPLICATION_CREDENTIALS="<a path to a Google service account JSON file>"
+export GOOGLE_CLOUD_PROJECT="<the name of the project that the service account above belongs to>"
+export FIREBASE_WEB_API_KEY="<an API key from the Firebase console for the project mentioned above>"
+
+# Go private modules
+export GOPRIVATE="gitlab.slade360emr.com/go/*,gitlab.slade360emr.com/optimalhealth/*"
+```
+
+The server deploys to Google Cloud Run. For Cloud Run, the necessary environment
+variables are:
+
+- `GOOGLE_CLOUD_PROJECT`
+- `FIREBASE_WEB_API_KEY`
--- server.go
+++ server.go
@@ -1,29 +1,112 @@
 package main
 
 import (
-	"log"
+	"compress/gzip"
+	"context"
 	"net/http"
 	"os"
+	"time"
+
+	log "github.com/sirupsen/logrus"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"gitlab.slade360emr.com/go/feed/graph"
-	"gitlab.slade360emr.com/go/feed/graph/generated"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"gitlab.slade360emr.com/go/base"
+	"gitlab.slade360emr.com/go/debug/graph"
+	"gitlab.slade360emr.com/go/debug/graph/generated"
 )
 
-const defaultPort = "8080"
+const serverTimeoutSeconds = 120
+
+var allowedOrigins = []string{
+	"https://healthcloud.co.ke",
+	"https://bewell.healthcloud.co.ke",
+	"http://localhost:5000",
+	"https://api-gateway-test.healthcloud.co.ke",
+	"https://api-gateway-prod.healthcloud.co.ke",
+	"https://debug-testing-uyajqt434q-ew.a.run.app",
+	"https://debug-prod-uyajqt434q-ew.a.run.app",
+}
+var allowedHeaders = []string{
+	"debug", "Accept", "Accept-Charset", "Accept-Language",
+	"Accept-Encoding", "Origin", "Host", "User-Agent", "Content-Length",
+	"Content-Type",
+}
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
+	ctx := context.Background()
+
+	err := base.Sentry()
+	if err != nil {
+		base.LogStartupError(ctx, err)
 	}
 
-	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
+	// start up the router
+	r, err := Router()
+	if err != nil {
+		base.LogStartupError(ctx, err)
+	}
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	// start the server
+	addr := ":" + base.MustGetEnvVar("PORT")
+	h := handlers.CompressHandlerLevel(r, gzip.BestCompression)
+	h = handlers.CORS(
+		handlers.AllowedHeaders(allowedHeaders),
+		handlers.AllowedOrigins(allowedOrigins),
+		handlers.AllowCredentials(),
+		handlers.AllowedMethods([]string{"OPTIONS", "GET", "POST"}),
+	)(h)
+	h = handlers.CombinedLoggingHandler(os.Stdout, h)
+	h = handlers.ContentTypeHandler(h, "application/json")
+	srv := &http.Server{
+		Handler:      h,
+		Addr:         addr,
+		WriteTimeout: serverTimeoutSeconds * time.Second,
+		ReadTimeout:  serverTimeoutSeconds * time.Second,
+	}
+	log.Fatal(srv.ListenAndServe())
+}
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+// Router sets up the ginContext router
+func Router() (*mux.Router, error) {
+	fc := &base.FirebaseClient{}
+	firebaseApp, err := fc.InitFirebase()
+	if err != nil {
+		return nil, err
+	}
+	r := mux.NewRouter() // gorilla mux
+	r.Use(
+		handlers.RecoveryHandler(
+			handlers.PrintRecoveryStack(true),
+			handlers.RecoveryLogger(log.StandardLogger()),
+		),
+	) // recover from panics by writing a HTTP error
+	r.Use(base.RequestDebugMiddleware())
+
+	// Unauthenticated routes
+	r.Path("/ide").HandlerFunc(playground.Handler("GraphQL IDE", "/graphql"))
+
+	// Authenticated routes
+	gqlR := r.Path("/graphql").Subrouter()
+	gqlR.Use(base.AuthenticationMiddleware(firebaseApp))
+	gqlR.Methods(
+		http.MethodPost, http.MethodGet, http.MethodOptions,
+	).HandlerFunc(graphqlHandler())
+	return r, nil
+
+}
+
+func graphqlHandler() http.HandlerFunc {
+	srv := handler.NewDefaultServer(
+		generated.NewExecutableSchema(
+			generated.Config{
+				Resolvers: graph.NewResolver(),
+			},
+		),
+	)
+	return func(w http.ResponseWriter, r *http.Request) {
+		srv.ServeHTTP(w, r)
+	}
 }
