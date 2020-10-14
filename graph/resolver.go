5a4b441ebea639410f9a4d486b23cceb271d5137
Mike Farad <mike18farad@gmail.com>
2020-10-14 17:37:05 +0300

    chore: Add Go Generate Annotation to resolver

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,6 @@
+//go:generate go run github.com/99designs/gqlgen
+
 package graph
 
-// This file will not be regenerated automatically.
-//
-// It serves as dependency injection for your app, add any dependencies you require here.
-
+// Resolver serves as dependency injection for your app, add any dependencies you require here.
 type Resolver struct{}
