74c4756410abfc81b9a5cf2278bfe7acce5a0cec
Salaton <elvisdenis056@gmail.com>
2021-02-17 15:51:20 +0300

    test: add acceptance tests

--- tests/acceptance_test.go
+++ tests/acceptance_test.go
@@ -1,10 +1,12 @@
 package tests
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/http/httputil"
 	"testing"
 	"time"
 
@@ -1788,6 +1790,572 @@ func TestGraphQLGetFeed(t *testing.T) {
 			}
 
 			assert.Equal(t, tt.wantStatus, resp.StatusCode)
+		})
+	}
+}
+
+func TestGraphQLUpload(t *testing.T) {
+
+	graphQLURL := fmt.Sprintf("%s/%s", baseURL, "graphql")
+	headers := getGraphQLHeaders(t)
+
+	graphqlMutation := `mutation upload($input: UploadInput!) {
+		upload(input: $input) {
+		  id
+		  url
+		  size
+		  hash
+		  creation
+		  title
+		  contentType
+		  language
+		  base64data
+		}
+	  }
+	`
+
+	type args struct {
+		query map[string]interface{}
+	}
+
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+		wantErr    bool
+	}{
+		{
+			name: "valid query",
+			args: args{
+				query: map[string]interface{}{
+					"query": graphqlMutation,
+					"variables": map[string]interface{}{
+						"input": map[string]interface{}{
+							"title":       "Upload title",
+							"contentType": "json",
+							"language":    "en",
+							"base64data":  "data",
+							"filename":    "filename",
+						},
+					},
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    false,
+		},
+		{
+			name: "invalid query - Using invalid payload",
+			args: args{
+				query: map[string]interface{}{
+					"query": graphqlMutation,
+					"variables": map[string]interface{}{
+						"title":       123456,
+						"contentType": 123456,
+						"language":    123456,
+						"base64data":  123456,
+						"filename":    123456,
+					},
+				},
+			},
+			wantStatus: http.StatusUnprocessableEntity,
+			wantErr:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			body, err := mapToJSONReader(tt.args.query)
+
+			if err != nil {
+				t.Errorf("unable to get GQL JSON io Reader: %s", err)
+				return
+			}
+
+			r, err := http.NewRequest(
+				http.MethodPost,
+				graphQLURL,
+				body,
+			)
+
+			if err != nil {
+				t.Errorf("unable to compose request: %s", err)
+				return
+			}
+
+			if r == nil {
+				t.Errorf("nil request")
+				return
+			}
+
+			for k, v := range headers {
+				r.Header.Add(k, v)
+			}
+			client := http.Client{
+				Timeout: time.Second * testHTTPClientTimeout,
+			}
+			resp, err := client.Do(r)
+			if err != nil {
+				t.Errorf("request error: %s", err)
+				return
+			}
+
+			dataResponse, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Errorf("can't read request body: %s", err)
+				return
+			}
+			if dataResponse == nil {
+				t.Errorf("nil response data")
+				return
+			}
+
+			data := map[string]interface{}{}
+			err = json.Unmarshal(dataResponse, &data)
+			if err != nil {
+				t.Errorf("bad data returned")
+				return
+			}
+
+			if tt.wantErr {
+				errMsg, ok := data["errors"]
+				if !ok {
+					t.Errorf("GraphQL error: %s", errMsg)
+					return
+				}
+			}
+
+			if !tt.wantErr {
+				_, ok := data["errors"]
+				if ok {
+					t.Errorf("error not expected")
+					return
+				}
+			}
+			if tt.wantStatus != resp.StatusCode {
+				b, _ := httputil.DumpResponse(resp, true)
+				t.Errorf("Bad status response returned; %v ", string(b))
+				return
+			}
+		})
+	}
+}
+
+func TestGraphQlGetLibraryContent(t *testing.T) {
+	graphQLURL := fmt.Sprintf("%s/%s", baseURL, "graphql")
+	headers := getGraphQLHeaders(t)
+
+	graphqlQuery := `query {
+		getLibraryContent {
+		  id
+		  slug
+		  uuid
+		  title
+		  html
+		  excerpt
+		  url
+		  featureImage
+		  readingTime
+		  tags {
+			id
+			name
+			slug
+			description
+			visibility
+			url
+		  }
+		  createdAt
+		  publishedAt
+		  updatedAt
+		  commentID
+		}
+	  }
+	`
+
+	type args struct {
+		query map[string]interface{}
+	}
+
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+		wantErr    bool
+	}{
+		{
+			name: "valid query",
+			args: args{
+				query: map[string]interface{}{
+					"query": graphqlQuery,
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    false,
+		},
+		{
+			name: "invalid query",
+			args: args{
+				query: map[string]interface{}{
+					"invalid": graphqlQuery,
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			body, err := mapToJSONReader(tt.args.query)
+
+			if err != nil {
+				t.Errorf("unable to get GQL JSON io Reader: %s", err)
+				return
+			}
+
+			r, err := http.NewRequest(
+				http.MethodPost,
+				graphQLURL,
+				body,
+			)
+
+			if err != nil {
+				t.Errorf("unable to compose request: %s", err)
+				return
+			}
+
+			if r == nil {
+				t.Errorf("nil request")
+				return
+			}
+
+			for k, v := range headers {
+				r.Header.Add(k, v)
+			}
+			client := http.Client{
+				Timeout: time.Second * testHTTPClientTimeout,
+			}
+			resp, err := client.Do(r)
+			if err != nil {
+				t.Errorf("request error: %s", err)
+				return
+			}
+
+			dataResponse, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Errorf("can't read request body: %s", err)
+				return
+			}
+			if dataResponse == nil {
+				t.Errorf("nil response data")
+				return
+			}
+
+			data := map[string]interface{}{}
+			err = json.Unmarshal(dataResponse, &data)
+			if err != nil {
+				t.Errorf("bad data returned")
+				return
+			}
+
+			if tt.wantErr {
+				errMsg, ok := data["errors"]
+				if !ok {
+					t.Errorf("GraphQL error: %s", errMsg)
+					return
+				}
+			}
+
+			if !tt.wantErr {
+				_, ok := data["errors"]
+				if ok {
+					t.Errorf("error not expected")
+					return
+				}
+			}
+			if tt.wantStatus != resp.StatusCode {
+				b, _ := httputil.DumpResponse(resp, true)
+				t.Errorf("Bad status response returned; %v ", string(b))
+				return
+			}
+		})
+	}
+}
+
+func TestGraphQlGetFaqsContent(t *testing.T) {
+	graphQLURL := fmt.Sprintf("%s/%s", baseURL, "graphql")
+	headers := getGraphQLHeaders(t)
+
+	graphqlQuery := `query {
+		getFaqsContent {
+		  id
+		  slug
+		  uuid
+		  title
+		  html
+		  excerpt
+		  url
+		  featureImage
+		  readingTime
+		  tags {
+			id
+			name
+			slug
+			description
+			visibility
+			url
+		  }
+		  createdAt
+		  publishedAt
+		  updatedAt
+		  commentID
+		}
+	  }
+	`
+
+	type args struct {
+		query map[string]interface{}
+	}
+
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+		wantErr    bool
+	}{
+		{
+			name: "valid query",
+			args: args{
+				query: map[string]interface{}{
+					"query": graphqlQuery,
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    false,
+		},
+		{
+			name: "invalid query",
+			args: args{
+				query: map[string]interface{}{
+					"invalid": graphqlQuery,
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			body, err := mapToJSONReader(tt.args.query)
+
+			if err != nil {
+				t.Errorf("unable to get GQL JSON io Reader: %s", err)
+				return
+			}
+
+			r, err := http.NewRequest(
+				http.MethodPost,
+				graphQLURL,
+				body,
+			)
+
+			if err != nil {
+				t.Errorf("unable to compose request: %s", err)
+				return
+			}
+
+			if r == nil {
+				t.Errorf("nil request")
+				return
+			}
+
+			for k, v := range headers {
+				r.Header.Add(k, v)
+			}
+			client := http.Client{
+				Timeout: time.Second * testHTTPClientTimeout,
+			}
+			resp, err := client.Do(r)
+			if err != nil {
+				t.Errorf("request error: %s", err)
+				return
+			}
+
+			dataResponse, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Errorf("can't read request body: %s", err)
+				return
+			}
+			if dataResponse == nil {
+				t.Errorf("nil response data")
+				return
+			}
+
+			data := map[string]interface{}{}
+			err = json.Unmarshal(dataResponse, &data)
+			if err != nil {
+				t.Errorf("bad data returned")
+				return
+			}
+
+			if tt.wantErr {
+				errMsg, ok := data["errors"]
+				if !ok {
+					t.Errorf("GraphQL error: %s", errMsg)
+					return
+				}
+			}
+
+			if !tt.wantErr {
+				_, ok := data["errors"]
+				if ok {
+					t.Errorf("error not expected")
+					return
+				}
+			}
+			if tt.wantStatus != resp.StatusCode {
+				b, _ := httputil.DumpResponse(resp, true)
+				t.Errorf("Bad status response returned; %v ", string(b))
+				return
+			}
+		})
+	}
+}
+
+func TestGraphQlGetLabels(t *testing.T) {
+	graphQLURL := fmt.Sprintf("%s/%s", baseURL, "graphql")
+	headers := getGraphQLHeaders(t)
+
+	graphqlQuery := `query($flavour: Flavour!) {
+		labels(flavour: $flavour)
+	  }
+	`
+
+	type args struct {
+		query map[string]interface{}
+	}
+
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus int
+		wantErr    bool
+	}{
+		{
+			name: "valid - Get labels on pro",
+			args: args{
+				query: map[string]interface{}{
+					"query": graphqlQuery,
+					"variables": map[string]interface{}{
+						"flavour": "PRO",
+					},
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    false,
+		},
+		{
+			name: "valid - Get labels on consumer",
+			args: args{
+				query: map[string]interface{}{
+					"query": graphqlQuery,
+					"variables": map[string]interface{}{
+						"flavour": "CONSUMER",
+					},
+				},
+			},
+			wantStatus: http.StatusOK,
+			wantErr:    false,
+		},
+		{
+			name: "invalid query",
+			args: args{
+				query: map[string]interface{}{
+					"query": graphqlQuery,
+					"variables": map[string]interface{}{
+						"invalid": "PRO",
+					},
+				},
+			},
+			wantStatus: http.StatusUnprocessableEntity,
+			wantErr:    true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			body, err := mapToJSONReader(tt.args.query)
+
+			if err != nil {
+				t.Errorf("unable to get GQL JSON io Reader: %s", err)
+				return
+			}
+
+			r, err := http.NewRequest(
+				http.MethodPost,
+				graphQLURL,
+				body,
+			)
+
+			if err != nil {
+				t.Errorf("unable to compose request: %s", err)
+				return
+			}
+
+			if r == nil {
+				t.Errorf("nil request")
+				return
+			}
+
+			for k, v := range headers {
+				r.Header.Add(k, v)
+			}
+			client := http.Client{
+				Timeout: time.Second * testHTTPClientTimeout,
+			}
+			resp, err := client.Do(r)
+			if err != nil {
+				t.Errorf("request error: %s", err)
+				return
+			}
+
+			dataResponse, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				t.Errorf("can't read request body: %s", err)
+				return
+			}
+			if dataResponse == nil {
+				t.Errorf("nil response data")
+				return
+			}
+
+			data := map[string]interface{}{}
+			err = json.Unmarshal(dataResponse, &data)
+			if err != nil {
+				t.Errorf("bad data returned")
+				return
+			}
+
+			if tt.wantErr {
+				errMsg, ok := data["errors"]
+				if !ok {
+					t.Errorf("GraphQL error: %s", errMsg)
+					return
+				}
+			}
+
+			if !tt.wantErr {
+				_, ok := data["errors"]
+				if ok {
+					t.Errorf("error not expected")
+					return
+				}
+			}
+			if tt.wantStatus != resp.StatusCode {
+				b, _ := httputil.DumpResponse(resp, true)
+				t.Errorf("Bad status response returned; %v ", string(b))
+				return
+			}
 		})
 	}
 }
