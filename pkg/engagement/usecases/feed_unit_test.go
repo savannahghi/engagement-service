869ac853a76e4266d78af8c2e032d627f908be62
Salaton <elvisdenis056@gmail.com>
2021-03-03 16:25:27 +0300

    test: add more feed unit tests

--- pkg/engagement/usecases/feed_unit_test.go
+++ pkg/engagement/usecases/feed_unit_test.go
@@ -1208,7 +1208,6 @@ func TestUnresolveFeedItem(t *testing.T) {
 				ctx:     ctx,
 				uid:     uid,
 				flavour: base.FlavourConsumer,
-				// itemID:  "",
 			},
 			wantErr: true,
 		},
@@ -1880,4 +1879,854 @@ func TestLabels(t *testing.T) {
 		})
 	}
 
+}
+
+func TestSaveLabel(t *testing.T) {
+	ctx := context.Background()
+
+	i, err := InitializeFakeEngagementInteractor()
+	if err != nil {
+		t.Errorf("failed to initialize the fake engagement interactor: %v", err)
+		return
+	}
+	uid := ksuid.New().String()
+
+	type args struct {
+		ctx     context.Context
+		uid     string
+		flavour base.Flavour
+		label   string
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    []string
+		wantErr bool
+	}{
+		{
+			name: "valid:successfully_save_valid_label",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+				label:   ksuid.New().String(),
+			},
+			want:    []string{common.DefaultLabel},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "valid:successfully_save_valid_label" {
+				fakeEngagement.SaveLabelFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					label string,
+				) error {
+					return nil
+				}
+			}
+			err := i.Feed.SaveLabel(tt.args.ctx, tt.args.uid, tt.args.flavour, tt.args.label)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FeedUseCaseImpl.TestSaveLabel() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr {
+				if err == nil {
+					t.Errorf("error expected got %v", err)
+					return
+				}
+			}
+			if !tt.wantErr {
+				if err != nil {
+					t.Errorf("error not expected got %v", err)
+					return
+				}
+			}
+		})
+	}
+}
+
+func TestUnreadPersistentItems(t *testing.T) {
+	ctx := context.Background()
+
+	i, err := InitializeFakeEngagementInteractor()
+	if err != nil {
+		t.Errorf("failed to initialize the fake engagement interactor: %v", err)
+		return
+	}
+	uid := ksuid.New().String()
+
+	type args struct {
+		ctx     context.Context
+		uid     string
+		flavour base.Flavour
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "valid:successfully_return_no_of_unread_inboxItems",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+			},
+			want:    0,
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "valid:successfully_return_no_of_unread_inboxItems" {
+				fakeEngagement.UnreadPersistentItemsFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+				) (int, error) {
+					return 0, nil
+				}
+			}
+			_, err := i.Feed.UnreadPersistentItems(tt.args.ctx, tt.args.uid, tt.args.flavour)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FeedUseCaseImpl.TestUnreadPersistentItems() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr {
+				if err == nil {
+					t.Errorf("error expected got %v", err)
+					return
+				}
+			}
+			if !tt.wantErr {
+				if err != nil {
+					t.Errorf("error not expected got %v", err)
+					return
+				}
+			}
+		})
+	}
+}
+
+func TestUpdateUnreadPersistentItemsCount(t *testing.T) {
+	ctx := context.Background()
+
+	i, err := InitializeFakeEngagementInteractor()
+	if err != nil {
+		t.Errorf("failed to initialize the fake engagement interactor: %v", err)
+		return
+	}
+	uid := ksuid.New().String()
+
+	type args struct {
+		ctx     context.Context
+		uid     string
+		flavour base.Flavour
+	}
+
+	tests := []struct {
+		name    string
+		args    args
+		want    int
+		wantErr bool
+	}{
+		{
+			name: "valid:successfully_update_no_of_unread_inboxItems",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+			},
+			want:    0,
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "valid:successfully_update_no_of_unread_inboxItems" {
+				fakeEngagement.UpdateUnreadPersistentItemsCountFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+				) error {
+					return nil
+				}
+			}
+			err := i.Feed.UpdateUnreadPersistentItemsCount(tt.args.ctx, tt.args.uid, tt.args.flavour)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FeedUseCaseImpl.TestUpdateUnreadPersistentItemsCount() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr {
+				if err == nil {
+					t.Errorf("error expected got %v", err)
+					return
+				}
+			}
+			if !tt.wantErr {
+				if err != nil {
+					t.Errorf("error not expected got %v", err)
+					return
+				}
+			}
+		})
+	}
+}
+
+func TestPublishNudge(t *testing.T) {
+	ctx := context.Background()
+
+	i, err := InitializeFakeEngagementInteractor()
+	if err != nil {
+		t.Errorf("failed to initialize the fake engagement interactor: %v", err)
+		return
+	}
+	uid := ksuid.New().String()
+	nudge := testNudge()
+	type args struct {
+		ctx     context.Context
+		uid     string
+		flavour base.Flavour
+		nudge   *base.Nudge
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "valid:successfully_publish_nudge",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+				nudge:   nudge,
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid:nil_nudge",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+				nudge:   nil,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid:fail_to_save_nudge",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+				nudge:   nudge,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid:fail_to_send_notification",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+				nudge:   nudge,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid:invalid_nudge",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+				nudge:   &base.Nudge{},
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid:invalid_nudgeActionType",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+				nudge: &base.Nudge{
+					ID: uuid.New().String(),
+					Actions: []base.Action{
+						{
+							ActionType: base.ActionTypeFloating,
+						},
+					},
+				},
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "valid:successfully_publish_nudge" {
+				fakeEngagement.SaveNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudge *base.Nudge,
+				) (*base.Nudge, error) {
+					return &base.Nudge{
+						ID:             uuid.New().String(),
+						SequenceNumber: 0,
+					}, nil
+				}
+
+				fakeMessaging.NotifyFn = func(
+					ctx context.Context,
+					topicID string,
+					uid string,
+					flavour base.Flavour,
+					payload base.Element,
+					metadata map[string]interface{},
+				) error {
+					return nil
+				}
+			}
+
+			if tt.name == "invalid:nil_nudge" {
+				fakeEngagement.SaveNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudge *base.Nudge,
+				) (*base.Nudge, error) {
+					return nil, fmt.Errorf("can't publish nil nudge")
+				}
+			}
+
+			if tt.name == "invalid:fail_to_save_nudge" {
+				fakeEngagement.SaveNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudge *base.Nudge,
+				) (*base.Nudge, error) {
+					return nil, fmt.Errorf("unable to publish nudge")
+				}
+			}
+
+			if tt.name == "invalid:fail_to_send_notification" {
+				fakeEngagement.SaveNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudge *base.Nudge,
+				) (*base.Nudge, error) {
+					return &base.Nudge{
+						ID:             uuid.New().String(),
+						SequenceNumber: 0,
+					}, nil
+				}
+
+				fakeMessaging.NotifyFn = func(
+					ctx context.Context,
+					topicID string,
+					uid string,
+					flavour base.Flavour,
+					payload base.Element,
+					metadata map[string]interface{},
+				) error {
+					return fmt.Errorf("failed to send notification")
+				}
+			}
+
+			if tt.name == "invalid:invalid_nudge" {
+				fakeEngagement.SaveNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudge *base.Nudge,
+				) (*base.Nudge, error) {
+					return nil, fmt.Errorf("invalid nudge")
+				}
+			}
+			if tt.name == "invalid:invalid_nudgeActionType" {
+				fakeEngagement.SaveNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudge *base.Nudge,
+				) (*base.Nudge, error) {
+					return &base.Nudge{
+						ID: uuid.New().String(),
+						Actions: []base.Action{
+							{
+								ActionType: base.ActionTypeFloating,
+							},
+						},
+					}, fmt.Errorf("invalid nudge")
+				}
+			}
+			got, err := i.Feed.PublishNudge(tt.args.ctx, tt.args.uid, tt.args.flavour, tt.args.nudge)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Feed.PublishNudge() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr {
+				if err == nil {
+					t.Errorf("error expected got %v", err)
+					return
+				}
+			}
+			if !tt.wantErr {
+				if err != nil {
+					t.Errorf("error not expected got %v", err)
+					return
+				}
+
+				if got == nil {
+					t.Errorf("nil item response returned")
+					return
+				}
+			}
+		})
+	}
+}
+
+func TestResolveNudge(t *testing.T) {
+	ctx := context.Background()
+
+	i, err := InitializeFakeEngagementInteractor()
+	if err != nil {
+		t.Errorf("failed to initialize the fake engagement interactor: %v", err)
+		return
+	}
+	uid := ksuid.New().String()
+	nudge := testNudge()
+	type args struct {
+		ctx     context.Context
+		uid     string
+		flavour base.Flavour
+		nudgeID string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus base.Status
+		wantErr    bool
+	}{
+		{
+			name: "valid:successfully_resolve_nudge",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+				nudgeID: nudge.ID,
+			},
+			wantStatus: base.StatusDone,
+			wantErr:    false,
+		},
+		{
+			name: "invalid:fail_to_get_nudge",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+				nudgeID: nudge.ID,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid:fail_to_update_nudge",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+				nudgeID: nudge.ID,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid:fail_to_send_notification",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+				nudgeID: nudge.ID,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "valid:successfully_resolve_nudge" {
+				fakeEngagement.GetNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudgeID string,
+				) (*base.Nudge, error) {
+					return &base.Nudge{
+						ID:             uuid.New().String(),
+						Status:         base.StatusDone,
+						SequenceNumber: 1,
+						Actions: []base.Action{
+							{
+								Name: common.ResolveItemActionName,
+							},
+						},
+					}, nil
+				}
+
+				fakeEngagement.UpdateNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudge *base.Nudge,
+				) (*base.Nudge, error) {
+					return &base.Nudge{
+						ID: uuid.New().String(),
+					}, nil
+				}
+
+				fakeMessaging.NotifyFn = func(
+					ctx context.Context,
+					topicID string,
+					uid string,
+					flavour base.Flavour,
+					payload base.Element,
+					metadata map[string]interface{},
+				) error {
+					return nil
+				}
+			}
+
+			if tt.name == "invalid:fail_to_get_nudge" {
+				fakeEngagement.GetNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudgeID string,
+				) (*base.Nudge, error) {
+					return nil, fmt.Errorf("unable to get nudge")
+				}
+			}
+
+			if tt.name == "invalid:fail_to_update_nudge" {
+				fakeEngagement.GetNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudgeID string,
+				) (*base.Nudge, error) {
+					return &base.Nudge{
+						ID:             uuid.New().String(),
+						Status:         base.StatusDone,
+						SequenceNumber: 1,
+						Actions: []base.Action{
+							{
+								Name: common.ResolveItemActionName,
+							},
+						},
+					}, nil
+				}
+
+				fakeEngagement.UpdateNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudge *base.Nudge,
+				) (*base.Nudge, error) {
+					return nil, fmt.Errorf("unable to resolve nudge")
+				}
+			}
+
+			if tt.name == "invalid:fail_to_send_notification" {
+				fakeEngagement.GetNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudgeID string,
+				) (*base.Nudge, error) {
+					return &base.Nudge{
+						ID:             uuid.New().String(),
+						Status:         base.StatusDone,
+						SequenceNumber: 1,
+						Actions: []base.Action{
+							{
+								Name: common.ResolveItemActionName,
+							},
+						},
+					}, nil
+				}
+
+				fakeEngagement.UpdateNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudge *base.Nudge,
+				) (*base.Nudge, error) {
+					return &base.Nudge{
+						ID: uuid.New().String(),
+					}, nil
+				}
+
+				fakeMessaging.NotifyFn = func(
+					ctx context.Context,
+					topicID string,
+					uid string,
+					flavour base.Flavour,
+					payload base.Element,
+					metadata map[string]interface{},
+				) error {
+					return fmt.Errorf("unable to send notification")
+				}
+			}
+			got, err := i.Feed.ResolveNudge(tt.args.ctx, tt.args.uid, tt.args.flavour, tt.args.nudgeID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Feed.ResolveNudge() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr {
+				if err == nil {
+					t.Errorf("error expected got %v", err)
+					return
+				}
+			}
+			if !tt.wantErr {
+				if err != nil {
+					t.Errorf("error not expected got %v", err)
+					return
+				}
+
+				if got == nil {
+					t.Errorf("nil nudge response returned")
+					return
+				}
+			}
+		})
+	}
+}
+
+func TestUnresolveNudge(t *testing.T) {
+	ctx := context.Background()
+
+	i, err := InitializeFakeEngagementInteractor()
+	if err != nil {
+		t.Errorf("failed to initialize the fake engagement interactor: %v", err)
+		return
+	}
+	uid := ksuid.New().String()
+	nudge := testNudge()
+	type args struct {
+		ctx     context.Context
+		uid     string
+		flavour base.Flavour
+		nudgeID string
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantStatus base.Status
+		wantErr    bool
+	}{
+		{
+			name: "valid:successfully_Unresolve_nudge",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+				nudgeID: nudge.ID,
+			},
+			wantStatus: base.StatusDone,
+			wantErr:    false,
+		},
+		{
+			name: "invalid:fail_to_get_nudge",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+				nudgeID: nudge.ID,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid:fail_to_update_nudge",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+				nudgeID: nudge.ID,
+			},
+			wantErr: true,
+		},
+		{
+			name: "invalid:fail_to_send_notification",
+			args: args{
+				ctx:     ctx,
+				uid:     uid,
+				flavour: base.FlavourConsumer,
+				nudgeID: nudge.ID,
+			},
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.name == "valid:successfully_Unresolve_nudge" {
+				fakeEngagement.GetNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudgeID string,
+				) (*base.Nudge, error) {
+					return &base.Nudge{
+						ID:             uuid.New().String(),
+						Status:         base.StatusDone,
+						SequenceNumber: 1,
+						Actions: []base.Action{
+							{
+								Name: common.UnResolveItemActionName,
+							},
+						},
+					}, nil
+				}
+
+				fakeEngagement.UpdateNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudge *base.Nudge,
+				) (*base.Nudge, error) {
+					return &base.Nudge{
+						ID: uuid.New().String(),
+					}, nil
+				}
+
+				fakeMessaging.NotifyFn = func(
+					ctx context.Context,
+					topicID string,
+					uid string,
+					flavour base.Flavour,
+					payload base.Element,
+					metadata map[string]interface{},
+				) error {
+					return nil
+				}
+			}
+
+			if tt.name == "invalid:fail_to_get_nudge" {
+				fakeEngagement.GetNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudgeID string,
+				) (*base.Nudge, error) {
+					return nil, fmt.Errorf("unable to get nudge")
+				}
+			}
+
+			if tt.name == "invalid:fail_to_update_nudge" {
+				fakeEngagement.GetNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudgeID string,
+				) (*base.Nudge, error) {
+					return &base.Nudge{
+						ID:             uuid.New().String(),
+						Status:         base.StatusDone,
+						SequenceNumber: 1,
+						Actions: []base.Action{
+							{
+								Name: common.UnResolveItemActionName,
+							},
+						},
+					}, nil
+				}
+
+				fakeEngagement.UpdateNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudge *base.Nudge,
+				) (*base.Nudge, error) {
+					return nil, fmt.Errorf("unable to Unresolve nudge")
+				}
+			}
+
+			if tt.name == "invalid:fail_to_send_notification" {
+				fakeEngagement.GetNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudgeID string,
+				) (*base.Nudge, error) {
+					return &base.Nudge{
+						ID:             uuid.New().String(),
+						Status:         base.StatusDone,
+						SequenceNumber: 1,
+						Actions: []base.Action{
+							{
+								Name: common.UnResolveItemActionName,
+							},
+						},
+					}, nil
+				}
+
+				fakeEngagement.UpdateNudgeFn = func(
+					ctx context.Context,
+					uid string,
+					flavour base.Flavour,
+					nudge *base.Nudge,
+				) (*base.Nudge, error) {
+					return &base.Nudge{
+						ID: uuid.New().String(),
+					}, nil
+				}
+
+				fakeMessaging.NotifyFn = func(
+					ctx context.Context,
+					topicID string,
+					uid string,
+					flavour base.Flavour,
+					payload base.Element,
+					metadata map[string]interface{},
+				) error {
+					return fmt.Errorf("unable to send notification")
+				}
+			}
+			got, err := i.Feed.UnresolveNudge(tt.args.ctx, tt.args.uid, tt.args.flavour, tt.args.nudgeID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Feed.UnresolveNudge() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr {
+				if err == nil {
+					t.Errorf("error expected got %v", err)
+					return
+				}
+			}
+			if !tt.wantErr {
+				if err != nil {
+					t.Errorf("error not expected got %v", err)
+					return
+				}
+
+				if got == nil {
+					t.Errorf("nil nudge response returned")
+					return
+				}
+			}
+		})
+	}
 }
