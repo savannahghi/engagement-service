a600bb6ff9f49b9dfb32d75326310b14887bbd5d
Salaton <salaton@healthcloud.co.ke>
2021-08-09 08:48:25 +0300

    tests: add mocking to services tests

---
+++ pkg/engagement/infrastructure/services/mail/mock/mail_mock.go
@@ -0,0 +1,89 @@
+package mock
+
+import (
+	"context"
+
+	"github.com/savannahghi/engagement/pkg/engagement/application/common/dto"
+)
+
+// FakeServiceMail defines a mock Mail service interface
+type FakeServiceMail struct {
+	SendInBlueFn  func(ctx context.Context, subject, text string, to ...string) (string, string, error)
+	SendMailgunFn func(
+		ctx context.Context,
+		subject, text string,
+		body *string,
+		to ...string,
+	) (string, string, error)
+	SendEmailFn func(
+		ctx context.Context,
+		subject, text string,
+		body *string,
+		to ...string,
+	) (string, string, error)
+	SimpleEmailFn func(
+		ctx context.Context,
+		subject, text string,
+		body *string,
+		to ...string,
+	) (string, error)
+	SaveOutgoingEmailsFn          func(ctx context.Context, payload *dto.OutgoingEmailsLog) error
+	UpdateMailgunDeliveryStatusFn func(
+		ctx context.Context,
+		payload *dto.MailgunEvent,
+	) (*dto.OutgoingEmailsLog, error)
+	GenerateEmailTemplateFn func(name string, templateName string) string
+}
+
+// SendInBlue ...
+func (f *FakeServiceMail) SendInBlue(ctx context.Context, subject, text string, to ...string) (string, string, error) {
+	return f.SendInBlueFn(ctx, subject, text, to...)
+}
+
+// SendMailgun ...
+func (f *FakeServiceMail) SendMailgun(
+	ctx context.Context,
+	subject, text string,
+	body *string,
+	to ...string,
+) (string, string, error) {
+	return f.SendMailgunFn(ctx, subject, text, body, to...)
+}
+
+// SendEmail ...
+func (f *FakeServiceMail) SendEmail(
+	ctx context.Context,
+	subject, text string,
+	body *string,
+	to ...string,
+) (string, string, error) {
+	return f.SendEmailFn(ctx, subject, text, body, to...)
+}
+
+// SimpleEmail ...
+func (f *FakeServiceMail) SimpleEmail(
+	ctx context.Context,
+	subject, text string,
+	body *string,
+	to ...string,
+) (string, error) {
+	return f.SimpleEmailFn(ctx, subject, text, body, to...)
+}
+
+// SaveOutgoingEmails ...
+func (f *FakeServiceMail) SaveOutgoingEmails(ctx context.Context, payload *dto.OutgoingEmailsLog) error {
+	return f.SaveOutgoingEmailsFn(ctx, payload)
+}
+
+// UpdateMailgunDeliveryStatus ...
+func (f *FakeServiceMail) UpdateMailgunDeliveryStatus(
+	ctx context.Context,
+	payload *dto.MailgunEvent,
+) (*dto.OutgoingEmailsLog, error) {
+	return f.UpdateMailgunDeliveryStatusFn(ctx, payload)
+}
+
+// GenerateEmailTemplate ...
+func (f *FakeServiceMail) GenerateEmailTemplate(name string, templateName string) string {
+	return f.GenerateEmailTemplateFn(name, templateName)
+}
--- pkg/engagement/infrastructure/services/twilio/mock/twilio_mock.go
+++ pkg/engagement/infrastructure/services/twilio/mock/twilio_mock.go
@@ -1,1 +1,49 @@
 package mock
+
+import (
+	"context"
+	"net/url"
+
+	"github.com/savannahghi/engagement/pkg/engagement/application/common/dto"
+)
+
+// FakeServiceTwilio defines the interaction with the twilio service
+type FakeServiceTwilio struct {
+	MakeTwilioRequestFn func(
+		method string,
+		urlPath string,
+		content url.Values,
+		target interface{},
+	) error
+
+	RoomFn func(ctx context.Context) (*dto.Room, error)
+
+	TwilioAccessTokenFn func(ctx context.Context) (*dto.AccessToken, error)
+
+	SendSMSFn func(ctx context.Context, to string, msg string) error
+}
+
+// MakeTwilioRequest ...
+func (f *FakeServiceTwilio) MakeTwilioRequest(
+	method string,
+	urlPath string,
+	content url.Values,
+	target interface{},
+) error {
+	return f.MakeTwilioRequestFn(method, urlPath, content, target)
+}
+
+// Room ...
+func (f *FakeServiceTwilio) Room(ctx context.Context) (*dto.Room, error) {
+	return f.RoomFn(ctx)
+}
+
+// TwilioAccessToken ...
+func (f *FakeServiceTwilio) TwilioAccessToken(ctx context.Context) (*dto.AccessToken, error) {
+	return f.TwilioAccessTokenFn(ctx)
+}
+
+// SendSMS ...
+func (f *FakeServiceTwilio) SendSMS(ctx context.Context, to string, msg string) error {
+	return f.SendSMSFn(ctx, to, msg)
+}
