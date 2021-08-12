13c6e7b853c1ccfd08f48aa8ac43e2b2cfb6ffa5
Dumbledore <mathenge@healthcloud.co.ke>
2021-08-12 08:39:52 +0300

    chore: update sms copy
    Signed-off-by: Dumbledore <mathenge@healthcloud.co.ke>

--- pkg/engagement/infrastructure/services/crm/service.go
+++ pkg/engagement/infrastructure/services/crm/service.go
@@ -3,6 +3,7 @@ package crm
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/savannahghi/engagement/pkg/engagement/application/common/helpers"
 	"github.com/savannahghi/engagement/pkg/engagement/infrastructure/services/mail"
@@ -16,8 +17,15 @@ var tracer = otel.Tracer("github.com/savannahghi/engagement/pkg/engagement/useca
 // ServiceCrm represents commontools crm lib usecases extension
 type ServiceCrm interface {
 	IsOptedOut(ctx context.Context, phoneNumber string) (bool, error)
-	CollectEmails(ctx context.Context, email string, phonenumber string) (*hubspotDomain.CRMContact, error)
-	BeWellAware(ctx context.Context, email string) (*hubspotDomain.CRMContact, error)
+	CollectEmails(
+		ctx context.Context,
+		email string,
+		phonenumber string,
+	) (*hubspotDomain.CRMContact, error)
+	BeWellAware(
+		ctx context.Context,
+		email string,
+	) (*hubspotDomain.CRMContact, error)
 }
 
 // Hubspot interacts with `HubSpot` CRM usecases
@@ -27,7 +35,10 @@ type Hubspot struct {
 }
 
 // NewCrmService inits a new crm instance
-func NewCrmService(hubSpotUsecases hubspotUsecases.HubSpotUsecases, mail mail.ServiceMail) *Hubspot {
+func NewCrmService(
+	hubSpotUsecases hubspotUsecases.HubSpotUsecases,
+	mail mail.ServiceMail,
+) *Hubspot {
 	return &Hubspot{
 		hubSpotUsecases: hubSpotUsecases,
 		mail:            mail,
@@ -35,29 +46,47 @@ func NewCrmService(hubSpotUsecases hubspotUsecases.HubSpotUsecases, mail mail.Se
 }
 
 // IsOptedOut checks if a given phone number is opted out
-func (h *Hubspot) IsOptedOut(ctx context.Context, phoneNumber string) (bool, error) {
+func (h *Hubspot) IsOptedOut(
+	ctx context.Context,
+	phoneNumber string,
+) (bool, error) {
 	return h.hubSpotUsecases.IsOptedOut(ctx, phoneNumber)
 }
 
 // CollectEmails receives an email and update the email of the found record in our firestore and hubspot CRM
-func (h *Hubspot) CollectEmails(ctx context.Context, email string, phonenumber string) (*hubspotDomain.CRMContact, error) {
+func (h *Hubspot) CollectEmails(
+	ctx context.Context,
+	email string,
+	phonenumber string,
+) (*hubspotDomain.CRMContact, error) {
 	ctx, span := tracer.Start(ctx, "CollectEmails")
 	defer span.End()
 
 	contact, err := h.hubSpotUsecases.GetContactByPhone(ctx, phonenumber)
 	if err != nil {
 		helpers.RecordSpanError(span, err)
-		return nil, fmt.Errorf("failed to get contact with phone number %s: %w", phonenumber, err)
+		return nil, fmt.Errorf(
+			"failed to get contact with phone number %s: %w",
+			phonenumber,
+			err,
+		)
 	}
 	if contact == nil {
-		return nil, fmt.Errorf("contact with phonenumber %s not found", phonenumber)
+		return nil, fmt.Errorf(
+			"contact with phonenumber %s not found",
+			phonenumber,
+		)
 	}
 	contact.Properties.Email = email
 
 	updatedContact, err := h.hubSpotUsecases.UpdateHubSpotContact(ctx, contact)
 	if err != nil {
 		helpers.RecordSpanError(span, err)
-		return nil, fmt.Errorf("failed to update contact with phone number %s: %w", phonenumber, err)
+		return nil, fmt.Errorf(
+			"failed to update contact with phone number %s: %w",
+			phonenumber,
+			err,
+		)
 	}
 
 	name := "Kevin From Be.Well"
@@ -71,21 +100,29 @@ func (h *Hubspot) CollectEmails(ctx context.Context, email string, phonenumber s
 		email,
 	)
 	if err != nil {
-		return contact, fmt.Errorf("failed to send welcome email to %s: %w", email, err)
+		// todo: restore one mailgun is back
+		log.Printf("failed to send welcome email to %s: %v", email, err)
 	}
 
 	return updatedContact, nil
 }
 
 //BeWellAware toggles the user identified by the provided email as bewell-aware
-func (h *Hubspot) BeWellAware(ctx context.Context, email string) (*hubspotDomain.CRMContact, error) {
+func (h *Hubspot) BeWellAware(
+	ctx context.Context,
+	email string,
+) (*hubspotDomain.CRMContact, error) {
 	ctx, span := tracer.Start(ctx, "BeWellAware")
 	defer span.End()
 
 	contact, err := h.hubSpotUsecases.GetContactByEmail(ctx, email)
 	if err != nil {
 		helpers.RecordSpanError(span, err)
-		return nil, fmt.Errorf("failed to get contact with email %s: %w", email, err)
+		return nil, fmt.Errorf(
+			"failed to get contact with email %s: %w",
+			email,
+			err,
+		)
 	}
 	if contact == nil {
 		return nil, fmt.Errorf("contact with email %s not found", email)
@@ -95,7 +132,11 @@ func (h *Hubspot) BeWellAware(ctx context.Context, email string) (*hubspotDomain
 	updatedContact, err := h.hubSpotUsecases.UpdateHubSpotContact(ctx, contact)
 	if err != nil {
 		helpers.RecordSpanError(span, err)
-		return nil, fmt.Errorf("failed to update contact with email %s: %w", email, err)
+		return nil, fmt.Errorf(
+			"failed to update contact with email %s: %w",
+			email,
+			err,
+		)
 	}
 	return updatedContact, nil
 }
