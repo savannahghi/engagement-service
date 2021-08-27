739aa00c829bc6f69ae89919bb3cc3d6d454b6f9
Otieno Calvine <nyarangaotieno@gmail.com>
2021-08-27 14:17:16 +0300

    chore: match service and library folder structure
    Signed-off-by: Otieno Calvine <nyarangaotieno@gmail.com>

--- pkg/engagement/usecases/usecases.go
+++ pkg/engagement/usecases/usecases.go
@@ -7,17 +7,22 @@ import (
 	openSource "github.com/savannahghi/engagement/pkg/engagement/usecases"
 )
 
+// Usecases is an interface that combines of all usescases
 type Usecases interface {
 	openSource.Usecases
 	// New usecases
+	// Just an example of how you can add new usecase
 	TestFeature(ctx context.Context) (bool, error)
 }
 
+//CombinedInteractor is used to combine both our internal and open source
+// usecases implementation
 type CombinedInteractor struct {
 	openSource.Usecases
 	InternalInteractor
 }
 
+// NewUsecasesInteractor initializes new combined usecases interactor
 func NewUsecasesInteractor(infrastructure infrastructure.Infrastructure) *CombinedInteractor {
 
 	open := openSource.NewUsecasesInteractor(infrastructure)
@@ -31,14 +36,17 @@ func NewUsecasesInteractor(infrastructure infrastructure.Infrastructure) *Combin
 	return impl
 }
 
+// InternalInteractor ...
 type InternalInteractor struct {
 	infrastructure infrastructure.Infrastructure
 }
 
+// NewInternalInteractor ...
 func NewInternalInteractor(infrastructure infrastructure.Infrastructure) *InternalInteractor {
 	return &InternalInteractor{infrastructure: infrastructure}
 }
 
+// TestFeature ...
 func (i InternalInteractor) TestFeature(ctx context.Context) (bool, error) {
 	return true, nil
 }
