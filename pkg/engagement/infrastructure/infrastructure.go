33de0eeac168f8a40f8ea925867673870edf5634
Otieno Calvine <35563516+NYARAS@users.noreply.github.com>
2021-08-27 19:29:56 +0300

    chore: match service and library folder structure (#63)
    Signed-off-by: Otieno Calvine <nyarangaotieno@gmail.com>

--- pkg/engagement/infrastructure/infrastructure.go
+++ pkg/engagement/infrastructure/infrastructure.go
@@ -2,18 +2,23 @@ package infrastructure
 
 import openSource "github.com/savannahghi/engagement/pkg/engagement/infrastructure"
 
+// Infrastructure is an interface that combines of all infrastructure
 type Infrastructure interface {
 	openSource.Infrastructure
-	// New usecases
 }
+
+//CombinedInteractor is used to combine both our internal and open source
+// infrastructure implementation
 type CombinedInteractor struct {
 	openSource.Infrastructure
 	InternalInteractor
 }
 
+// NewInfrastructureInteractor initializes new combined interactor
 func NewInfrastructureInteractor() *CombinedInteractor {
-
+	// open is open source interactor that both internal and open source services depend on
 	open := openSource.NewInfrastructureInteractor()
+	// internal is internal interactor that our internal services depends on
 	internal := NewInternalInteractor()
 
 	impl := &CombinedInteractor{open, *internal}
@@ -21,11 +26,14 @@ func NewInfrastructureInteractor() *CombinedInteractor {
 	return impl
 }
 
+// InternalInteractor ...
 type InternalInteractor struct {
 }
 
+// NewInternalInteractor initializes new internal interactor
 func NewInternalInteractor() *InternalInteractor {
 	return &InternalInteractor{}
 }
 
+// DummyInfrastructureFunction ...
 func (i InternalInteractor) DummyInfrastructureFunction() {}
