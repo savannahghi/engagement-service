47c752a471abecb697cf752aafa1bbfe1440e6ff
Kenneth Mathenge <mathenge362@gmail.com>
2021-09-13 10:32:19 +0300

    chore: add test coverage threshold and update badges (#73)
    Signed-off-by: Dumbledore <mathenge362@gmail.com>

---
+++ scripts/coverage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Quality Gate: checking test coverage is above threshold ..."
+echo "Threshold             : ${TESTCOVERAGE_THRESHOLD} %"
+totalCoverage=`go tool cover -func=coverage.out | grep total | grep -Eo '[0-9]+\.[0-9]+'`
+echo "Current test coverage : $totalCoverage %"
+if (( $(echo "$totalCoverage ${TESTCOVERAGE_THRESHOLD}" | awk '{print ($1 >= $2)}') )); then
+    echo "OK"
+else
+    echo "Current test coverage is below threshold. Please add more unit tests or adjust threshold to a lower value."
+    echo "Failed"
+    exit 1
+fi
